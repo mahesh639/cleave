8da12dba69a72b04ac546650ff3d487c78efaec7
hrithik <hrithik086@gmail.com>
2024-09-22 12:25:53 +0530

    deleted login and signup component inside app directory

--- splitapp/src/app/app.module.ts
+++ splitapp/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
